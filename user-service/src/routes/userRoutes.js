d69c7a29ee0ce66f5e73c2b69cbaf8891b2e179d
Madhawa Awishka <madhawaawishka@gmail.com>
2025-04-19 22:08:04 +0530

    Add user management routes: get user by ID, delete user, and update user

--- user-service/src/routes/userRoutes.js
+++ user-service/src/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { getUsers, createUser } = require("../controllers/userController");
+const { getUsers, createUser,getUserById,deleteUserById,updateUserById } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.post("/", createUser);
+router.get("/:userId", getUserById);
+router.delete("/:userId", deleteUserById);
+router.put("/:userId", updateUserById);
+
 
 module.exports = router;
