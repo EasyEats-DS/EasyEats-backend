4a78747e6b88d3c04a6628d34a0e72923c08c18d
Madhawa Awishka <madhawaawishka@gmail.com>
2025-04-21 23:00:05 +0530

    Add restaurant routes for CRUD operations and menu item management

---
+++ resturant-service/src/routes/resturantRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  createRestaurant,
+  getRestaurantById,
+  updateRestaurant,
+  getRestaurantsByOwner,
+  addMenuItem,
+  updateMenuItem
+} = require("../controllers/resturantController");
+
+const router = express.Router();
+
+router.post("/", createRestaurant);
+router.get("/:id", getRestaurantById);
+router.put("/:id", updateRestaurant);
+router.get("/owner/:ownerId", getRestaurantsByOwner);
+router.post("/:id/menu", addMenuItem);
+router.put("/:id/menu/:menuItemId", updateMenuItem);
+
+module.exports = router;
