ef5106fb2e63287b324de59b7f0c55ff553819df
danuka-nalidu <danukanalidu2002@gmail.com>
2025-04-21 19:21:17 +0530

    Update getOrderById route to use descriptive parameter name

--- order-service/src/routes/orderRoutes.js
+++ order-service/src/routes/orderRoutes.js
@@ -4,6 +4,6 @@ const { createOrder, getOrderById } = require("../controllers/orderController");
 const router = express.Router();
 
 router.post("/", createOrder);
-router.get("/:id", getOrderById); 
+router.get("/:orderId", getOrderById); 
 
 module.exports = router;
