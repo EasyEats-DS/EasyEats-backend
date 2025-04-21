925cae39908051b831fb72deaec3ccdc78b71879
Madhawa Awishka <madhawaawishka@gmail.com>
2025-04-21 22:58:33 +0530

    Add restaurantRoutes to API Gateway for handling restaurant-related requests

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const userRoutes = require("./routes/userRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const restaurantRoutes = require("./routes/resturantRoutes");
 const { initKafkaProducer, initKafkaConsumer } = require("./services/kafkaService");
 
 const app = express();
@@ -13,6 +14,8 @@ app.use(express.json());
 // Routes
 app.use("/users", userRoutes);
 app.use("/orders", orderRoutes);
+app.use("/restaurants", restaurantRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Connected to API Gateway");
