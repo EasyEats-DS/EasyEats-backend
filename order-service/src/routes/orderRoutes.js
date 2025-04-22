a0d524bc96664dae0ef29965674693e0998fec4c
danuka-nalidu <danukanalidu2002@gmail.com>
2025-04-22 23:53:48 +0530

    Add getOrdersByUserId route to orderRoutes

--- order-service/src/routes/orderRoutes.js
+++ order-service/src/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { createOrder, getOrderById, getOrders, updateOrderStatus, deleteOrderById, updateOrder } = require("../controllers/orderController");
+const { createOrder, getOrderById, getOrders, updateOrderStatus, deleteOrderById, updateOrder, getOrdersByUserId } = require("../controllers/orderController");
 
 const router = express.Router();
 
 router.post("/", createOrder);
 router.get('/', getOrders);
 router.get("/:orderId", getOrderById); 
+router.get('/user/:userId', getOrdersByUserId);
 router.put('/:id', updateOrder);
 router.put('/:id/status', updateOrderStatus);
 router.delete('/:id', deleteOrderById);
