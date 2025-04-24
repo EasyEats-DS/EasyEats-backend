15d9aaa93aabd52d5965ae2e249b9298088a277f
Madhawa Awishka <madhawaawishka@gmail.com>
2025-04-24 15:21:37 +0530

    feat: create User model with Mongoose schema

---
+++ authentication-service/src/models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zipCode: String,
+    country: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
