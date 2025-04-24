8e2a1bd6f014a4918da5ec64df03d5cae3e16c1b
Madhawa Awishka <madhawaawishka@gmail.com>
2025-04-24 15:21:15 +0530

    feat: implement MongoDB connection setup in db.js

---
+++ authentication-service/src/config/db.js
@@ -0,0 +1,27 @@
+// authentication-service/config/db.js
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+  const mongoURI = process.env.MONGO_URI;
+  console.log("MONGO_URI:", mongoURI);
+  
+  if (!mongoURI) {
+    console.error("MONGO_URI is not defined!");
+    throw new Error("MongoDB connection string is missing");
+  }
+
+  try {
+    await mongoose.connect(mongoURI, {
+      serverSelectionTimeoutMS: 5000,
+      retryWrites: true
+    });
+    console.log("Service connected to MongoDB");
+    console.log("Connected to database:", mongoose.connection.db.databaseName); 
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
