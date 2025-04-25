e4d692b5b14829b1ac0d8be4ebaf9759b4538cd4
Madhawa Awishka <madhawaawishka@gmail.com>
2025-04-25 09:50:58 +0530

    feat: create user model with schema definition and role management

---
+++ api-gateway/src/models/userModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['ADMIN', 'RESTAURANT_OWNER', 'DELIVERY_PERSON','CUSTOMER'],
+    default: 'CUSTOMER',
+    required: true,
+    // default: 'DELIVERY_PERSON' 
+  },
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zipCode: String,
+    country: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
